2d1e9bfd9d3e38db2a344e6535cdb310a59fcca9
LucasHAlbergaria <lucashenriquealbergaria01@gmail.com>
2026-05-24 11:05:11 -0300

    Criado a classe AFD, mas ainda está incompleto

---
+++ TrabalhoFinal/AFD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoFinal
+{
+    internal class AFD
+    {
+
+        public HashSet<string> estados;
+
+        public HashSet<char> entrada;
+
+        public Dictionary<(string estado, char simbolo), string> delta;
+
+        public string q0;
+
+        public HashSet<string> Final;
+
+        public AFD()
+        {
+            estados = new HashSet<string>{"q0", "q1", "q2",};
+            entrada = new HashSet<char> {'a', 'b'};
+            delta = new Dictionary<(string estado, char simbolo), string> 
+            {
+                { ("q0", 'a'), "q1" },
+                { ("q0", 'b'), "q0" },
+                { ("q1", 'a'), "q1" },
+                { ("q1", 'b'), "q2" },
+                { ("q1", 'a'), "q1" },
+
+
+            };
+            q0 = "q0";
+            Final = new HashSet<string> {"q3"};
+
+        }
+
+
+
+
+
+    }
+}
--- TrabalhoFinal/Program.cs
+++ TrabalhoFinal/Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Menu m = new Menu();
+        m.Exibir();
+        m.LerOpcao();
+        m.Executar();
     }
 }
